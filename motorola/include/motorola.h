059fa30eec300be8dc9b4d61d902674d79fd1658
David Dobo <daweskie@gmail.com>
2013-11-13 10:53:01 +0100

    changed motorola.h

--- motorola/include/motorola.h
+++ motorola/include/motorola.h
@@ -1,20 +1,7 @@
 #ifndef MOTOROLA_H_INCLUDED
 #define MOTOROLA_H_INCLUDED
 
-/*
- * Copyright (C)  2011 Zoltan Zidarics (Zamek)
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/** \file motorola.h
-    \brief Motorola frame handler for serial
-
+/** 
     Frame format:
 
     n*0x55      (MOTOROLA_PREAMBLE_COUNT*MOTOROLA_PREAMBLE)
@@ -27,36 +14,33 @@
     crc         (2 byte little endian (LH) format)
 */
 
-/**
-    Config keys for motorola module
-*/
-#define MOTOROLA_KEY_PREAMBLE "preamble.char"
+
+#define MOTOROLA_PREAMBLE "preamble.char"
 #define MOTOROLA_DEF_PREAMBLE 0x55
 
-#define MOTOROLA_KEY_PREAMBLE_COUNTS "preamble.counts"
+#define MOTOROLA_PREAMBLE_COUNTS "preamble.counts"
 #define MOTOROLA_DEF_PREAMBLE_COUNTS 1
 
-#define MOTOROLA_KEY_MTU "mtu"
+#define MOTOROLA_MTU "mtu"
 #define MOTOROLA_DEF_MTU 32
 
-#define MOTOROLA_KEY_FRAME_QUEUE_SIZE "frame-queue.size"
+#define MOTOROLA_FRAME_QUEUE_SIZE "frame-queue.size"
 #define MOTOROLA_DEF_FRAME_QUEUE_SIZE 5
 
 
-#include <highlevel.h>
+
 #include <motorola.h>
-#include <crc.h>
-#include <config_common.h>
 
-struct hls_data_t *motorola_init(char *serial_id);
 
-struct hls_data_t *motorola_init_by_direct(char *serial_id,
+struct adat *MotInit(char *serial_id);
+
+struct adat *MotInitDirect(char *serial_id,
                                            int queue_size, int mtu,
                                            char preamble, int preamble_counts);
 
-void motorola_receive_char(struct hls_data_t *hls_data);
+void MotReceive(struct adat *data);
 
-int motorola_send_frame(struct hls_data_t *hls_data,
+int MotSend(struct adat *data,
                          int address, int command,
                          char *buffer, int data_length);
 
